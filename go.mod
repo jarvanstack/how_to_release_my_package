3ae9f560abeab1bc49466ec65903098e7241c9cd
jarvan <dengjiawen2530@qq.com>
2021-05-30 10:11:59 +0800

    init

---
+++ go.mod
@@ -0,0 +1,1 @@
+module "github.com/dengjiawen8955/how_to_release_my_package"
---
+++ hello_test.go
@@ -0,0 +1,10 @@
+package how_to_release_my_package
+
+import "fmt"
+
+//github.com/dengjiawen8955/how_to_release_my_package
+
+// Hi says Hi Here , my first package..
+func Hi()  {
+	fmt.Printf("%s\n", "Hi Here , my first package.")
+}
